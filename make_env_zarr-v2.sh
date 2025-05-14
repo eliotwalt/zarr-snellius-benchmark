a1618e97c180fe0d47b9093b8ca754266249859b
eliotwalt <walt.eliot@hotmail.com>
2025-05-14 10:00:36 +0200

    fix zarr v2 env

--- make_env_zarr-v2.sh
+++ make_env_zarr-v2.sh
@@ -1,4 +1,7 @@
 python3.11 -m venv env/venv_zarr-v2
 
 source env/venv_zarr-v2/bin/activate
-pip install "xarray[complete]" torch torchaudio torchvision gcsfs PyYAML
+which python
+
+pip install "xarray[complete]" torch torchaudio torchvision gcsfs PyYAML ipykernel "zarr<3"
+python -c "import torch, gcsfs, xarray, zarr ; print('zarr version:', zarr.__version__)"
--- make_env_zarr-v3.sh
+++ make_env_zarr-v3.sh
@@ -1,4 +1,7 @@
 python3.11 -m venv env/venv_zarr-v3
 
 source env/venv_zarr-v3/bin/activate
-pip install "xarray[complete]" torch torchaudio torchvision gcsfs PyYAML zarr~=3.0.0
+which python
+
+pip install "xarray[complete]" torch torchaudio torchvision gcsfs PyYAML zarr~=3.0.0 ipykernel
+python -c "import torch, gcsfs, xarray, zarr ; print('zarr version:', zarr.__version__)"
